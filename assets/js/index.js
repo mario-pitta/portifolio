83c528301cb9872680516cbbd3070e8165ede9a4
Mario Pitta <pitta_mario@hotmail.com>
2022-11-02 13:39:42 -0300

    init commit

---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+function alertar() {
+    alert('ok')
+}
---
+++ assets/js/typed.js
@@ -0,0 +1,11 @@
+var options = {
+    strings: [
+        'Fullstack Node Dev.',
+        'FrontEnd Techlead.',
+        'Scrum Master.',
+        'Dev passionate! ❤'
+    ],
+    typeSpeed: 100
+};
+
+var typed = new Typed('.element', options);
