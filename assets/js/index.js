dd4275017cd335c1af15bca6918bae306ded6522
Mario Pitta <pitta_mario@hotmail.com>
2023-08-09 01:13:32 -0300

    ready to deploy - responsivity  ready

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,15 @@ function alertar() {
 $(function() {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+const btn = document.getElementById('btnCollapseNavMenu');
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const navMenu = document.getElementById('navMenu');
+    navMenu.classList.add('collapsing')
+    setTimeout(() => {
+        navMenu.classList.toggle('show')
+    }, 80)
+    navMenu.classList.remove('collapsing')
+})
